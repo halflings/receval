98ac89cf50daebf8bcecc6218992f76cc09d2c72
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-11-13 13:43:59 +0100

    Added a baseline (average) recommender + some validation on recommendations

---
+++ receval/__init__.py
@@ -0,0 +1,10 @@
+"""
+    RecEval: a framework for evaluating recommender systems
+"""
+
+from . import preprocessing, splitter, download, evaluation, metrics
+
+__title__ = 'receval'
+__version__ = '0.1'
+__author__ = 'Ahmed Kachkach'
+__license__ = 'Apache 2.0'
--- receval/recommender.py
+++ receval/recommender.py
@@ -7,7 +7,7 @@ import pandas as pd
 class Recommender(object):
     """
         Base class for recommender classes.
-        Recommenders should implement a recommend function that takes training ratings
+        Recommenders should implement a `_recommend` function that takes training ratings
         and test ratings (both dataframes) and returns a dataframe containin the predicted
         ratings for the (user, item) pairs present in the test set.
     """
@@ -16,8 +16,24 @@ class Recommender(object):
             raise ValueError("Provided data_dir does not exist or is a file.")
         self.data_dir = data_dir
 
+    def _validate_recommendations(self, predicted, test):
+        predicted_users = set(predicted.user)
+        test_users = set(test.user)
+        missing_users = test_users - predicted_users
+        if missing_users:
+            raise ValueError("Recommendations missing for {} user(s) of the test set.".format(len(missing_users)))
+        extra_users = predicted_users - test_users
+        if extra_users:
+            raise ValueError("Recommendations present for {} user(s) that were not in the test set.".format(len(extra_users)))
+
     def recommend(self, train_ratings, test_ratings):
-        return NotImplemented
+        # TODO: some validation for the train and test data
+        recommendations = self._recommend(train_ratings, test_ratings)
+        self._validate_recommendations(recommendations, test_ratings)
+        return recommendations
+
+    def _recommend(self, train_ratings, test_ratings):
+        raise NotImplementedError("The `_recommend` method has to be implemented by all recommenders.")
 
 class CommandRecommender(Recommender):
     """
@@ -35,7 +51,7 @@ class CommandRecommender(Recommender):
         super(CommandRecommender, self).__init__(*args, **kwargs)
         self.recommender_cmd = recommender_cmd
 
-    def recommend(self, train_ratings, test_ratings):
+    def _recommend(self, train_ratings, test_ratings):
         evaluation_id = str(uuid4())
 
         train_path = os.path.join(self.data_dir, '{}_test'.format(evaluation_id))
@@ -57,7 +73,7 @@ class CommandRecommender(Recommender):
 
 class DummyRecommender(Recommender):
     """A recommender that simply returns an exact copy of the expected ratings"""
-    def recommend(self, train_ratings, test_ratings):
+    def _recommend(self, train_ratings, test_ratings):
         return test_ratings.copy().reset_index(drop=True)
 
 class DummyCommandRecommender(CommandRecommender):
@@ -67,20 +83,40 @@ class DummyCommandRecommender(CommandRecommender):
     def __init__(self, *args, **kwargs):
         super(DummyCommandRecommender, self).__init__(self.DUMMY_EVALUATOR_CMD, *args, **kwargs)
 
+class AverageBaselineRecommender(Recommender):
+    """
+        Useful as a baseline, this recommender rates each item
+        with its average rating in the training set.
+    """
+    def _recommend(self, train_ratings, test_ratings):
+        avg_ratings = train_ratings[['item', 'rating']].groupby('item', as_index=False).mean()
+        users_df = train_ratings[['user']].drop_duplicates()
+
+        # Cartesian product between the users dataframe and the average ratings
+        avg_ratings.set_index([[0]*len(avg_ratings)], inplace=True)
+        users_df.set_index([[0]*len(users_df)], inplace=True)
+        recommendations = users_df.join(avg_ratings, how='outer')
+
+        return recommendations
 
 def main():
-    import pandas as pd
-
     from splitter import RandomSplitter
+    from download import load_movielens
 
     dummy_cmd = DummyCommandRecommender()
     dummy_obj = DummyRecommender()
-    df = pd.read_csv('data/tiny.csv')
+    df = load_movielens()
     splitter = RandomSplitter(0.5, per_user=True)
+    print("* Splitting...")
     train, test = splitter.split(df)
 
     print(dummy_cmd.recommend(train, test))
+    print("* Dummy object...")
     print(dummy_obj.recommend(train, test))
+
+    rec = AverageBaselineRecommender()
+    recommendations = rec.recommend(train, test)
+    print(recommendations)
 
 if __name__ == '__main__':
     main()
